846043c307d96321123ce3022d89974b3e263ce4
LongTran0919 <hoanglong090320@gmail.com>
2021-09-29 15:57:30 +0700

    Login/signup Page

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Footer from './Component/Footer/index';
+import Login from './Component/LoginPage/Login'
+import SignUp from './Component/SignupPage/SingUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <SignUp/>
+          </Route>
+          <Route path="/footer">
+            <Footer/>
+          </Route>
+    </Router>
   );
 }
 
