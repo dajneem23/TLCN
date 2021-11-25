479d90be7f123a7a307a2aab1e91a1a4acb3a5a2
Le Duc Thang <67146524+ldthang2201@users.noreply.github.com>
2021-11-25 08:18:04 +0700

    201 update ip for aws

--- Client/src/Service/axios.js
+++ Client/src/Service/axios.js
@@ -1,6 +1,8 @@
 import axiosBase from 'axios'
 import dotenv from 'dotenv'
 dotenv.config()
+const AWS_URL = 'http://18.211.249.250:4000';
+const HOST = 'http://localhost:4000';
 const axios =axiosBase.create({
     withCredentials :true,
     //this is devlopment host
